df0188d9ca3a79b68f11276e587e6561ed000f13
xxljunjun <liaojunjun@autel.com>
2021-09-23 19:46:38 +0800

    新的apk

---
+++ src/components/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class=""></view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
